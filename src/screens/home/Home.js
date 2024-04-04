bc6e9e99d001871103d8d051762b0109d96b8c68
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-04-04 21:43:20 +0900

    #

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -9,7 +9,7 @@ import wallet from './img/Wallet_745515.svg'
 function Home() {
     return (
         <div class="home">
-            <div class="container">
+            <div class="container-suit">
                 <div class="home-items ">
                     <img src={home} alt='home' />
                     <div class="context">
@@ -18,37 +18,40 @@ function Home() {
                         <h1>Products</h1>
                     </div>
                 </div>
-                <div class="home-support">
-                    <ul>
-                        <li>
+                <div class="home-support mt-5 ">
+                    <div class='raw border border-danger'>
+                        <div class="col-md-3 text-center border border-danger">
                             <img src={delivery} alt='delivery' />
                             <div>
                                 <h4>Free shipping</h4>
                                 <p>Free shipping on all orders</p>
                             </div>
-                        </li>
-                        <li>
+                        </div>
+                        <div class="col-md-3 text-center">
                             <img src={guarantee} alt='guarantee' />
                             <div>
                                 <h4>Return Guarantee</h4>
                                 <p>30 day money back</p>
                             </div>
-                        </li>
-                        <li>
+                        </div>
+                        <div class="col-md-3 text-center">
                             <img src={group} alt='group' />
                             <div>
                                 <h4>Online support 24/7</h4>
                                 <p>Technical support 24/7</p>
                             </div>
-                        </li>
-                        <li>
+                        </div>
+                        <div class="col-md-3 text-center">
                             <img src={wallet} alt='wallet' />
                             <div>
                                 <h4>Secure payment</h4>
                                 <p>All payment methods accepted</p>
                             </div>
-                        </li>
-                    </ul>
+                        </div>
+                    </div>
+                    {/* <div class="raw">
+
+                    </div> */}
                 </div>
             </div>
         </div>
---
+++ src/screens/home/Winter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Winter() {
+    return (
+        <div class="winter">
+            <div class="container">
+                <div class="winter-items">
+                    <img src='' alt='winter'/>
+                    <div class='winter-context'>
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Winter;
