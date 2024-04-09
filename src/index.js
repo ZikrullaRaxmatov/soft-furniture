6b81bde05e7beffe7d38b0636110a820c4a0a728
Zikrulla-Rakhmatov <zikrulloraxmatov98@gmail.com>
2024-04-10 08:15:32 +0900

    #

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import i18next from 'i18next'
+import global_eng from './translaction/Eng/global.json'
+import global_uzb from './translaction/Uzb/global.json'
+import global_kor from './translaction/Kor/global.json'
+import './index.css';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_eng,
+    },
+
+    uz: {
+      global: global_uzb,
+    },
+
+    ko: {
+      global: global_kor,
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <I18nextProvider i18n={i18next} >
+      <App />
+    </I18nextProvider>
   </BrowserRouter>
 );
 
